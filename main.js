b97b36447a9bd6e155b6240827def2b10a2ddd07
JakubDrobnik <drobnik.j@gmail.com>
2018-05-25 15:52:18 +0200

    Add basic SEO analysis

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const appsJson = require('wappalyzer/apps.json');
 const dns = require('dns');
 const { promisify } = require('util');
 
-const MAX_PAGE_PER_DOMAIN = 3;
+// const MAX_PAGE_PER_DOMAIN = 3;
 
 const dnsLookup = promisify(dns.lookup);
 const dnsResolve6 = promisify(dns.resolve6);
@@ -99,6 +99,75 @@ const pageTechnologyAnalyzer = async (pageResponse, page) => {
     return Object.values(foundTechnologise);
 };
 
+const basicSEOAnalysis = async (page) => {
+    await Apify.utils.puppeteer.injectJQuery(page);
+    const analysis = await page.evaluate(() => {
+        const result = {};
+        const PARAMS = {
+            maxTitleLength: 70,
+            minTitleLength: 10,
+            maxMetaDescriptionLength: 140,
+            maxLinksCount: 3000,
+            maxWordsCount: 350,
+        };
+        // -- Google Analytics
+        // check for GA Object (e.g crawler can not find function(i,s,o,g,r,a,m) in meteor page like Apifier)
+        result.isGoogleAnalyticsObject = (typeof ga !== 'undefined') ? true : false;
+        // check for GA function (function(i,s,o,g,r,a,m)) exists in page
+        result.isGoogleAnalyticsFunc = ($("script:contains(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject'])").length) ? true : false;
+        // -- Meta charset
+        result.isCharacterEncode = ($("meta[charset]")) ? true : false;
+        // -- Meta description
+        result.isMetaDescription = ($('meta[name=description]').length || $('meta[name=description]').length) ? true : false;
+        if (result.isMetaDescription) {
+            result.metaDescription = $('meta[name=description]').attr("content") || $('meta[name=description]').attr("content");
+            result.isMetaDescriptionLong = (result.metaDescription.length > PARAMS.maxMetaDescriptionLength) ? true : false;
+        }
+        // --  Doctype
+        result.isDoctype = (document.doctype) ? true : false;
+        // -- Title
+        if ($("title").length) {
+            result.isTitle = true;
+            result.title = $("title").text();
+            const titleLength = result.title.length;
+            result.isTitleLong = (titleLength > PARAMS.maxTitleLength) ? true : false;
+            result.isTitleShort = (titleLength < PARAMS.minTitleLength) ? true : false;
+        } else result.isTitle = false;
+        // -- h1
+        const h1Count = $("h1").length;
+        result.isH1 = (h1Count > 0) ? true : false;
+        if (result.isH1) result.h1 = $("h1").text();
+        result.isH1Multiple = (h1Count > 1) ? true : false;
+        // -- h2
+        result.isH2 = ($("h2").length) ? true : false;
+        // -- Links
+        result.linksCount = $("a").length;
+        result.isTooMuchlinks = (result.linksCount > PARAMS.maxLinksCount) ? true : false;
+        result.internalNoFollowLinks = [];
+        $("a").each(function() {
+            if ($(this).attr("rel") == "nofollow" && this.href.indexOf(window.location.hostname) > -1) result.internalNoFollowLinks.push(this.href);
+        });
+        result.internalNoFollowLinksCount = result.internalNoFollowLinks.length;
+        // -- images
+        result.notOptimizedImgs = [];
+        $("img:not([alt])").each(function() {
+            result.notOptimizedImgs.push($(this).attr("src"));
+        });
+        result.notOptimizedImgsCount = result.notOptimizedImgs.length;
+        // -- words count
+        result.wordsCount = $("body").text().match(/\S+/g).length;
+        result.isContentTooLong = (result.wordsCount > PARAMS.maxWordsCount) ? true : false;
+        // -- viewport
+        result.isViewport = ($('meta[name=viewport]')) ? true : false;
+        // -- amp version if page
+        result.isAmp = ($('html[⚡]') || $('html[amp]')) ? true : false;
+        // -- iframe check
+        result.isIframe = ($('iframe').length) ? true : false;
+        return result;
+    });
+    return analysis;
+};
+
 Apify.main(async () => {
     // Get queue and enqueue first url.
     const requestQueue = await Apify.openRequestQueue();
@@ -126,7 +195,7 @@ Apify.main(async () => {
         requestList,
         requestQueue,
         minConcurrency: (Apify.isAtHome()) ? 20 : 1,
-        launchPuppeteerOptions: { apifyProxyGroups: ['SHADER'] },
+        launchPuppeteerOptions: { apifyProxyGroups: ['CZECH_LUMINATI'] },
 
 
         handlePageFunction: async ({ page, request }) => {
@@ -185,7 +254,8 @@ Apify.main(async () => {
                 // No data for IPv6 lookup
             }
 
-
+            // Basic SEO analysis
+            const basicSEO = await basicSEOAnalysis(page);
 
             await Apify.pushData({
                 url: page.url(),
@@ -197,6 +267,8 @@ Apify.main(async () => {
                 technologies: technologyLookupResults,
                 serverIPv4address,
                 isIPv6Support,
+                isSSLRedirect: (homePageUrl.protocol === 'https:'),
+                basicSEO,
             });
 
             console.log(`Finish analysis for ${request.url}`);
